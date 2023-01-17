f2864576b64fef14c687ab5f99890e1d2e1076ab
AshVance <93738991+AshVance@users.noreply.github.com>
2023-01-17 12:59:58 +0000

    Create CarTaxSys.cs

---
+++ January/CarTaxSys.cs
@@ -0,0 +1,34 @@
+using System;
+
+// This work is from the L1 OOP Basics powerpoint.
+namespace CarTaxSystem
+{
+    class CarTaxSys
+    {
+        #region Main
+        static void Main(string[] args)
+        {
+            Car CarOne = new Car(); // Creates a new instance of the Car class.
+            // Class properties:
+            // car_model (string)
+            // car_make (string)
+            // fuel_type (string)
+            // emissions (integer)
+
+            // The user inputs the various attributes of their car.
+            Console.Write("Please input the car's model: ");
+            CarOne.car_model = Console.ReadLine();
+            Console.Write("Please input the car's manufacturer: ");
+            CarOne.car_make = Console.ReadLine();
+            Console.Write("Please input whether it is diesel, petrol or electric: ");
+            CarOne.fuel_type = Console.ReadLine();
+            Console.Write("Please input the car's emission levels (0 - 100): ");
+            CarOne.emissions = int.Parse(Console.ReadLine());
+
+            int tax_owed = CarOne.CalculateTax(); // This run the CalculateTax function in the Car class, which returns the tax owed.
+
+            Console.Write($"\nFor your {CarOne.car_make} {CarOne.car_model} (fuel type: {CarOne.fuel_type}), you owe £{tax_owed}.\n");
+        }
+        #endregion
+    }
+}
