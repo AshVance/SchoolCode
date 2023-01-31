1b90118cfd610f3e88dc03a9a4211e699ac7264d
AshVance <93738991+AshVance@users.noreply.github.com>
2023-01-31 13:18:44 +0000

    Create FlavourTexts.cs

---
+++ TextBasedGame/FlavourTexts.cs
@@ -0,0 +1,12 @@
+namespace ConsoleApp3
+{
+    class FlavourTexts
+    {
+        public static string[] flavour_texts_attack = { "You lunge at the enemy, catching its arm. It staggers back in pain.\n", "Your flying fist meets the enemy's chest, striking their windpipe and stunning them.",
+            "You go for a kick, hitting them square in the face.\n", "Your flailing arm somehow collides with them, and somehow it even hurts them.\n" };
+
+        public static string[] flavour_texts_dodge = { };
+
+        public static string[] flavour_texts_run = { };
+    }
+}
