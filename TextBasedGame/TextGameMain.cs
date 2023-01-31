c352141e03a296f07cd70531ae0a75aaed0f982c
AshVance <93738991+AshVance@users.noreply.github.com>
2023-01-31 10:59:17 +0000

    Create TextGameMain.cs

---
+++ TextBasedGame/TextGameMain.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+/* Enemy.cs Class Parameters:
+Holds weapon (bool) - hold_weapon
+Headshot only (bool) - headshot_only
+Is alive (bool) - headshot_only
+Health (integer) - health
+*/
+
+namespace TextGame
+{
+    class TextGameMain
+    {
+        static void Main(string[] args)
+        {
+            Enemy zombie = new Enemy();
+            zombie.HoldWeapon = false;
+            zombie.HeadshotOnly = false;
+            zombie.IsAlive = true;
+            zombie.Health = 100;
+
+            while ([INSTANCE NAME].IsAlive == true)
+            {
+                
+            }
+        }
+    }
+}
