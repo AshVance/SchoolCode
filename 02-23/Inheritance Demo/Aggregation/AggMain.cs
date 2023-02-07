4ad93e0081b5b598c1d429d2e17a3d4842884eb0
AshVance <93738991+AshVance@users.noreply.github.com>
2023-02-07 13:12:11 +0000

    Update AggMain.cs

--- 02-23/Inheritance Demo/Aggregation/AggMain.cs
+++ 02-23/Inheritance Demo/Aggregation/AggMain.cs
@@ -1,9 +1,5 @@
 // Work from "Programming paradigms.docx"
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AggregationTest
 {
