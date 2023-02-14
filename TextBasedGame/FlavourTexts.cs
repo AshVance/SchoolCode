64528fe6f5395b9428a0ab102d44f26807a8594f
AshVance <93738991+AshVance@users.noreply.github.com>
2023-02-14 10:55:34 +0000

    Update FlavourTexts.cs

--- TextBasedGame/FlavourTexts.cs
+++ TextBasedGame/FlavourTexts.cs
@@ -2,11 +2,20 @@ namespace TextGame
 {
     class FlavourTexts
     {
-        public static string[] flavour_texts_attack = { "You lunge at the enemy, catching its arm. It staggers back in pain.\n", "Your flying fist meets the enemy's chest, striking their windpipe and stunning them.",
-            "You go for a kick, hitting them square in the face.\n", "Your flailing arm somehow collides with them, and somehow it even hurts them.\n" };
+        public static string[] ft_attack = {
+            "You lunge at the enemy, catching its arm. It staggers back in pain.\n",
+            "Your flying fist meets the enemy's chest, striking their windpipe and stunning them.",
+            "You go for a kick, hitting them square in the face.\n",
+            "Your flailing arm somehow collides with them, and somehow it even hurts them.\n" };
 
-        public static string[] flavour_texts_dodge = { };
+        public static string[] ft_dodge_success = {
+            "Success! You dodge the alien's attack!",
+            "You valiently jump out of the aliens grasp, stifling its attack.",
+            "You swat the alien's hand away, in a somehow successful, yet still pathetic, attempt to stop its attack.",
+            "You perform a sick backflip to dodge the alien's attack. Shame no one else saw it." };
 
-        public static string[] flavour_texts_run = { };
+        public static string[] ft_dodge_fail = { "Uh oh. The alien's strike catches you in", "string1", "string2", "string3" };
+
+        public static string[] ft_run = { };
     }
 }
